01db533f416ebaf623c95b6fc21f5971e0d526c7
Anthony Cieri <anthony.cieri@hotmail.com>
2024-11-10 01:28:08 -0800

    feat: Line stuff

--- Assets/Scripts/GazeManager/GazeLineManager.cs
+++ Assets/Scripts/GazeManager/GazeLineManager.cs
@@ -8,6 +8,11 @@ public class GazeLineManager : BaseLineManager
     [SerializeField]
     private LineRenderer _lineRenderer;
 
+    private Vector3 _prevPosition;
+
+    [SerializeField]
+    private float _slerpSpeed;
+
 
 
     public override void ClearTemporaryLine()
@@ -26,7 +31,15 @@ public class GazeLineManager : BaseLineManager
 
     public override void DrawTemporaryLine(Vector3 prevStarPoint, Vector3 lookPoint)
     {
-        _lineRenderer.positionCount = 2;
+        if (_lineRenderer.positionCount == 0)
+        {
+            _lineRenderer.positionCount = 2;
+            _prevPosition = lookPoint;
+        }
+
+        lookPoint = Vector3.Lerp(_prevPosition, lookPoint, _slerpSpeed);
+        _prevPosition = lookPoint;
+
         _lineRenderer.SetPosition(0, prevStarPoint);
         _lineRenderer.SetPosition(1, lookPoint);
     }
