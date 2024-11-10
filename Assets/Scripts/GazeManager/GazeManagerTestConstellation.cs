17e6ad61be2227888710d2cda34423df07fff348
Anthony Cieri <anthony.cieri@hotmail.com>
2024-11-10 04:57:29 -0800

    feat: Spherical transformations

--- Assets/Scripts/GazeManager/GazeManagerTestConstellation.cs
+++ Assets/Scripts/GazeManager/GazeManagerTestConstellation.cs
@@ -8,13 +8,36 @@ public class GazeManagerTestConstellation : MonoBehaviour, IConstellation
     public GameObject GazeManager;
     public Vector3? PrevStarPosition { get; private set; }
 
+    public StarGroup StarGroup;
+
+    public float Scale;
+    public float Rotation;
+    public float Radius;
+    public Transform Center;
+
 
     public readonly List<GameObject> Stars = new();
 
 
     void Start()
     {
+        StarGroup = new(Radius);
+
         SpawnRandomStars(20);
+    }
+
+
+
+    void Update()
+    {
+        StarGroup.Radius = Radius;
+        StarGroup.ReCenter(Center.rotation * Vector3.forward);
+        List<Vector3> scaledStars = StarGroup.ScaledRotatedTranslated(Scale, Rotation * (Time.time + Mathf.Sin(Time.time * 0.1f)), Center.position + 2.0f * Vector3.up);
+
+        for (int i = 0; i < Stars.Count; i++)
+        {
+            Stars[i].transform.position = scaledStars[i];
+        }
     }
 
 
@@ -25,8 +48,10 @@ public class GazeManagerTestConstellation : MonoBehaviour, IConstellation
         for (int _ = 0; _ < num; _++)
         {
             GameObject newStar = Instantiate(StarPrefab);
-            Vector2 angles = new(Random.Range(0.0f, 24.0f), Random.Range(-90.0f, 90.0f));
-            newStar.transform.position = 5.0f * StarAligner.TransformAngles(angles);
+            Vector2 angles = new(Random.Range(10.0f, 18.0f), Random.Range(-40.0f, 10.0f));
+            Vector3 pos = StarAligner.TransformAngles(angles);
+            newStar.transform.position = Radius * pos;
+            StarGroup.StarPositions.Add(pos);
             Stars.Add(newStar);
             starComponents.Add(newStar.GetComponent<IStar>());
         }
--- Assets/Scripts/StarAlignment/StarGroup.cs
+++ Assets/Scripts/StarAlignment/StarGroup.cs
@@ -14,27 +14,69 @@ public class StarGroup
     public float Radius;
 
 
-    public StarGroup(List<Vector3> positions, float radius)
+
+    public StarGroup(float radius = 1.0f)
     {
-        StarPositions = positions;
+        StarPositions = new();
         Radius = radius;
     }
 
 
-    public void Scale(float factor)
+
+    public StarGroup(List<Vector3> positions, float radius = 1.0f)
     {
+        StarPositions = positions;
+        StarPositions.ForEach(p => p = p.normalized);
+        Radius = radius;
+    }
+
+
+    public void ReCenter(Vector3 center)
+    {
+        center = center.normalized;
         Vector3 com = CenterOfMassSurface();
+        if ((center - com).sqrMagnitude < 0.01f) return;
+
+        Vector3 poleVector = Vector3.Cross(com, center).normalized;
+        float distance = Vector3.Angle(com, center);
+        Quaternion travel = Quaternion.AngleAxis(distance, poleVector);
+
+        for (int posIndex = 0; posIndex < StarPositions.Count; posIndex++)
+        {
+            Vector3 rotatedPos = travel * StarPositions[posIndex];
+            StarPositions[posIndex] = rotatedPos;
+        }
+    }
+
+
+
+    /// <summary>
+    /// Dark magic (mathematics). I did this at 4:30 in the morning don't ask too many questions please.
+    /// </summary>
+    /// <param name="scale"></param>
+    /// <param name="angle"></param>
+    /// <param name="translation"></param>
+    /// <returns></returns>
+    public List<Vector3> ScaledRotatedTranslated(float scale, float angle, Vector3 translation)
+    {
+        List<Vector3> transformedPositions = new();
+
+        Vector3 com = CenterOfMassSurface();
+
+        Quaternion rotation = Quaternion.AngleAxis(angle, com);
 
         for (int posIndex = 0; posIndex < StarPositions.Count; posIndex++)
         {
             Vector3 pos = StarPositions[posIndex];
             float distance = Vector3.Angle(com, pos);
-            float scaledDistance = distance * factor;
+            float scaledDistance = distance * scale;
             Vector3 poleVector = Vector3.Cross(com, pos).normalized;
             Quaternion travel = Quaternion.AngleAxis(scaledDistance, poleVector);
-            Vector3 scaledPos = travel * com;
-            StarPositions[posIndex] = scaledPos;
+            Vector3 scaledPos = rotation * travel * com;
+            transformedPositions.Add(translation + (Radius * scaledPos));
         }
+
+        return transformedPositions;
     }
 
 
