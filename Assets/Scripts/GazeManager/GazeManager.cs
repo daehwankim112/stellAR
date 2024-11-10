2e8697f0960655fc38a4431c1d843b16f6de1d13
Anthony Cieri <anthony.cieri@hotmail.com>
2024-11-09 16:34:46 -0800

    feat: Star timers

--- Assets/Scripts/GazeManager/GazeManager.cs
+++ Assets/Scripts/GazeManager/GazeManager.cs
@@ -6,11 +6,19 @@ using UnityEngine;
 
 public class GazeManager : MonoBehaviour, IGazeManager
 {
-    private readonly List<Vector3> _starPositions = new();
-    private readonly List<IStar> _stars = new();
+    private readonly List<StarData> _stars = new();
 
     [SerializeField]
     private float _cosineAngle;
+
+    [SerializeField]
+    private float _selectTime;
+
+    [SerializeField]
+    private Transform _centerEyeTransform;
+
+    public IConstellation Constellation;
+    
     public float LookAngle
     {
         get
@@ -23,23 +31,25 @@ public class GazeManager : MonoBehaviour, IGazeManager
         }
     }
 
-    [SerializeField]
-    private Transform _centerEyeTransform;
-
-    public IConstellation Constellation;
 
 
-
-    void Update()
+    void LateUpdate()
     {
         Vector3 centerPos = _centerEyeTransform.position;
         Vector3 lookDirection = _centerEyeTransform.rotation * Vector3.forward;
 
-        for (int starIndex = 0; starIndex < _stars.Count; starIndex++)
+        foreach (var star in _stars)
         {
-            if (Vector3.Dot((_starPositions[starIndex] - centerPos).normalized, lookDirection) < _cosineAngle)
+            if (Vector3.Dot((star.Position - centerPos).normalized, lookDirection) < _cosineAngle)
             {
-                Constellation.LookAt(_stars[starIndex]);
+                Constellation.LookingAt(star.Star);
+                star.Timer += Time.deltaTime;
+
+                if (star.Timer >= _selectTime)
+                {
+                    Constellation.Selected(star.Star);
+                    star.Timer = 0.0f;
+                }
             }
         }
     }
@@ -48,7 +58,6 @@ public class GazeManager : MonoBehaviour, IGazeManager
 
     public void GiveStarList(IStar[] stars)
     {
-        _starPositions.Clear();
         _stars.Clear();
         
         foreach (IStar star in stars)
@@ -57,10 +66,25 @@ public class GazeManager : MonoBehaviour, IGazeManager
             {
                 if (starMB.TryGetComponent(out Transform transform))
                 {
-                    _starPositions.Add(transform.position);
-                    _stars.Add(star);
+                    _stars.Add(new(star, transform.position));
                 }
             }
+        }
+    }
+
+
+
+    private class StarData
+    {
+        public IStar Star;
+        public Vector3 Position;
+        public float Timer;
+
+        public StarData(IStar star, Vector3 position)
+        {
+            Star = star;
+            Position = position;
+            Timer = 0.0f;
         }
     }
 }
