e81d4c5ead319e4b1ec8d31e14327e9151104ae9
Anthony Cieri <anthony.cieri@hotmail.com>
2024-11-09 16:21:00 -0800

    feat: Gaze detection

--- Assets/Scripts/GazeManager/GazeManager.cs
+++ Assets/Scripts/GazeManager/GazeManager.cs
@@ -2,27 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class GazeManager : MonoBehaviour, IGazeManager
 {
     private readonly List<Vector3> _starPositions = new();
+    private readonly List<IStar> _stars = new();
 
-
-
-    void Start()
+    [SerializeField]
+    private float _cosineAngle;
+    public float LookAngle
     {
-        
+        get
+        {
+            return Mathf.Acos(_cosineAngle);
+        }
+        set
+        {
+            _cosineAngle = Mathf.Cos(value);
+        }
     }
 
-    // Update is called once per frame
+    [SerializeField]
+    private Transform _centerEyeTransform;
+
+    public IConstellation Constellation;
+
+
+
     void Update()
     {
-        
+        Vector3 centerPos = _centerEyeTransform.position;
+        Vector3 lookDirection = _centerEyeTransform.rotation * Vector3.forward;
+
+        for (int starIndex = 0; starIndex < _stars.Count; starIndex++)
+        {
+            if (Vector3.Dot((_starPositions[starIndex] - centerPos).normalized, lookDirection) < _cosineAngle)
+            {
+                Constellation.LookAt(_stars[starIndex]);
+            }
+        }
     }
+
 
 
     public void GiveStarList(IStar[] stars)
     {
         _starPositions.Clear();
+        _stars.Clear();
         
         foreach (IStar star in stars)
         {
@@ -31,6 +58,7 @@ public class GazeManager : MonoBehaviour, IGazeManager
                 if (starMB.TryGetComponent(out Transform transform))
                 {
                     _starPositions.Add(transform.position);
+                    _stars.Add(star);
                 }
             }
         }
